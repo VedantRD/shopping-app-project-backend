5783e87991d1918db4c8207fb8247bbf86727603
VedantRD <vedant.debadwar@gmail.com>
2025-02-04 20:47:16 +0530

    changed vercel configuration again

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string, {});
-        console.log("MongoDB connected");
+        console.log("MongoDB connected ");
     } catch (err) {
         console.error(err);
         process.exit(1); // Exit process with failure
